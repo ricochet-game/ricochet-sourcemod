37c8561ee57310a12ebb29c383cf9685c7d05001
Zobris <Zobris@db549501-b2b5-4eee-ab1f-020bfad0b556>
2010-01-18 16:43:09 +0000

    git-svn-id: svn://localhost/tmp/repo1/trunk@87 db549501-b2b5-4eee-ab1f-020bfad0b556

--- run_mod.bat
+++
@@ -1,1 +0,0 @@
-@"c:\games\steam\steam.exe" -applaunch 320 -dev -game "c:\games\steam\steamapps\SourceMods\Ricochet-Source" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
--- run_studiomdl.bat
+++
@@ -1,2 +0,0 @@
-@"c:\games\steam\steamapps\jimmy.headdon@btopenworld.com\sourcesdk\bin\studiomdl.exe" -game "c:\games\steam\steamapps\SourceMods\Ricochet-Source" %1 %2 %3 %4 %5 %6 %7 %8 %9
-
