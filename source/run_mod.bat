72e946cf7688da3a7564ac6a2a8c2bf243e59297
xiourtil <xiourtil@db549501-b2b5-4eee-ab1f-020bfad0b556>
2011-01-06 01:08:07 +0000

    Adding untouched sdk code base.
    git-svn-id: svn://localhost/tmp/repo1/trunk@160 db549501-b2b5-4eee-ab1f-020bfad0b556

---
+++ source/run_mod.bat
@@ -0,0 +1,1 @@
+@"c:\steam\steamapps\xiourtil\source sdk base 2007\hl2.exe" -dev -game "c:\steam\steamapps\SourceMods\ricochet" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ source/run_studiomdl.bat
@@ -0,0 +1,2 @@
+@"c:\steam\steamapps\xiourtil\sourcesdk\bin\source2007\bin\studiomdl.exe" -game "c:\steam\steamapps\SourceMods\ricochet" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
